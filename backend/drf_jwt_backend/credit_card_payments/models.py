0fa97ef26757e79eac942e27802fa08632257cc7
PepdaBass <peplinskijeffrey0@gmail.com>
2022-03-21 22:12:45 -0400

    feat: Successfully tested CRUD functions in postman. Edited fields in customers models.

--- backend/drf_jwt_backend/credit_card_payments/models.py
+++ backend/drf_jwt_backend/credit_card_payments/models.py
@@ -6,8 +6,8 @@ from customers.models import Customer
 class Credit_Card_Payment(models.Model):
     customer = models.ForeignKey(Customer, blank=True, null=True, on_delete=models.CASCADE)
     cc_type = models.CharField(max_length=30)
-    cc_number = models.IntegerField(max_length=16)
+    cc_number = models.CharField(max_length=16)
     cc_exp_date = models.CharField(max_length=5)
-    cc_cvv = models.IntegerField(max_length=3)
+    cc_cvv = models.CharField(max_length=3)
     cc_full_name = models.CharField(max_length=30)
     is_valid = models.BooleanField()
