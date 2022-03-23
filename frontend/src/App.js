53c515e9dc6b635e770d6a36176c00fa6a895d9d
PepdaBass <peplinskijeffrey0@gmail.com>
2022-03-23 17:25:54 -0400

    feat: Registration for new customers added. Working on the survey that points a customer to the boxes they are most likely to want.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 // General Imports
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -6,6 +7,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ContinueRegistrationPage from "./pages/ContinueRegistrationPage/ContinueRegistrationPage";
+import SurveyPage from "./pages/SurveyPage/SurveyPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -14,7 +17,12 @@ import Footer from "./components/Footer/Footer";
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
 
-function App() {
+
+export default function App() {
+  
+  const [surveyData, setSurveyData] = useState([]);
+  console.log(surveyData)
+
   return (
     <div>
       <Navbar />
@@ -29,10 +37,16 @@ function App() {
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/create_customer" element={
+          <PrivateRoute>
+            <ContinueRegistrationPage />
+          </PrivateRoute>} />
+        <Route path="/take_survey" element={
+          <PrivateRoute>
+             <SurveyPage setSurveyData={setSurveyData} />
+          </PrivateRoute>} />
       </Routes>
       <Footer />
     </div>
   );
 }
-
-export default App;
--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   const { registerUser } = useContext(AuthContext);
@@ -68,7 +69,7 @@ const RegisterPage = () => {
           NOTE: Make this an uncommon password with characters, numbers, and
           special characters!
         </p>
-        <button>Register!</button>
+          <button>Register</button>
       </form>
     </div>
   );
